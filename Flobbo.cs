0b41b7104d4a3a97e4708e9a287e2506dc2f8cf1
robe <robe@robe.com>
2024-02-05 17:42:27 -0600

    Agregar archivos de proyecto.

---
+++ Flobbo.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreamWriter_Magnets
+{
+    class Flobbo
+    {
+        private string zap;
+        public Flobbo(string zap)
+        {
+            this.zap = zap;
+        }
+
+        public StreamWriter Snobbo()
+        {
+            return new StreamWriter("macaw.txt");
+        }
+
+        public bool Blobbo(StreamWriter sw)
+        {
+            sw.WriteLine(zap);
+            zap = "green pruple";
+            return false;
+        }
+        public bool Blobbo(bool Already, StreamWriter sw)
+        {
+            if (Already)
+            {
+                sw.WriteLine(zap);
+                sw.Close();
+                return false;
+            }
+            else
+            {
+                sw.WriteLine(zap);
+                zap = "green pruple";
+                return true;
+            }
+            
+        }
+    }
+}
